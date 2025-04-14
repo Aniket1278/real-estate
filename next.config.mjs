1464c1c90c0683a5eff1193cd49f95e97e6b76a7
Aniket1278 <aniket12078@gmail.com>
2025-04-15 00:08:25 +0530

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["rtipspolyluwvbvemqym.supabase.co", "img.clerk.com"],
+    domains: ["cdjihjmcwacrzsyrfuaa.supabase.co", "img.clerk.com"],
   },
 };
 
